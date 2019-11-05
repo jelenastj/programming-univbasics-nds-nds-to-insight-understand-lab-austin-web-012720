6d062edab89420475670cd9aebe8ddbe4a5319ad
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-05 21:39:23 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,7 @@ def print_first_directors_movie_titles
   row_index= 0 
   movies = directors_database[0][:movies]
  while row_index<movies.count do
-   puts movies[row_index]
+   puts movies[row_index][:title]
    row_index+=1
  end
 end
